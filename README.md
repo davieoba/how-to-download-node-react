4de711491fa5d437da9d23fb6a7231f9edd6486e
davieoba <bodunrindavidbond@gmail.com>
2023-07-09 12:35:22 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+in the react component, a user should install the following package, the code to implement the download feature from the client is in the `app.js` file.
+
+```bash
+js-file-download
+```
---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+
+app.listen(4000, () => {
+  console.log(`application started on port 4000`)
+})
+
+
+/**
+ * I guess the way it is supposed to work is the server is sent a GET request from the request it will have the name of the file or a unique identifier like the file 'ID' it wants to download, occassionally the file may be stored in a storage container like s3 bucket, so the location url of the storage location will be in the database, my server will now make another request to the storage server to send the file, I can now process it from my server and send it to the user.
+ */
+
+app.get('/download', async (req, res) => {
+  try {
+    res.download('./sample.pdf')
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+
+/**
+ * this is a query
+ * /download?filename=sample&ext=pdf
+ */
+
+app.get('/download/:filename', async (req, res) => {
+  try {
+    const query = req.query
+    const params = req.params
+    console.log({ query }, { params })
+
+    res.download(`./${query.name}.${query.ext}`)
+  } catch (err) {
+    // some code
+  }
+})
+
+/**
+ * const download = async (e) => {
+    e.preventDefault()
+    // const response = await fetch('http://localhost:4000/download', {
+    //   method: 'GET'
+    // })
+
+    const response = await fetch('http://localhost:4000/download/filename?name=shadowboard&ext=jpg', {
+      method: 'GET'
+    })
+
+    const blob = await response.blob()
+
+    FileDownload(blob, 'shadowboard.jpg')
+  }
+ */
